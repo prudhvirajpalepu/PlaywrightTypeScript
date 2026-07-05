12b0ada74cad55a106cfbb981ae602b74721e769
prudhvirajpalepu <palepuprudhvi5@gmail.com>
2026-07-05 14:44:46 +0530

    contrlflowstmts

---
+++ dist/controlFlow/DoWhile.js
@@ -0,0 +1,6 @@
+"use strict";
+let k = 11;
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
---
+++ dist/controlFlow/Switch.js
@@ -0,0 +1,20 @@
+"use strict";
+let grade = 'A';
+switch (grade) {
+    case 'A':
+        {
+            console.log('excellent');
+            break;
+        }
+    case 'B':
+        {
+            console.log('Good');
+            break;
+        }
+    case 'C':
+        {
+            console.log('Fair');
+            break;
+        }
+    default: console.log('please pass correct grade');
+}
