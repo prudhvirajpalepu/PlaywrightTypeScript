b56d41e6a37406f7a3ec6844d8c5c548bfd5682b
prudhvirajpalepu <palepuprudhvi5@gmail.com>
2026-07-07 11:56:15 +0530

    returntypeinFunctions

---
+++ dist/Functions/returnInFun.js
@@ -0,0 +1,8 @@
+"use strict";
+//create a function which accept two numbers
+//it will return the addtion of two numbers
+function returnAddtion(a, b) {
+    return a + b;
+}
+let result = returnAddtion(10, 20);
+console.log(result);
